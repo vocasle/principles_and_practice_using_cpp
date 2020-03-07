d4bbf0f0233bde78ec8833fb4dff5fb288ace8b2
Nikita Elsakov <elsakov@teamidea.ru>
2020-03-07 10:28:42 +0300

    Add chapter 3 problem 7 solution file
    - Move function from chapter 3 problem 6 to header file

--- part_1_the_basics/chap_3_probl_6.cpp
+++ part_1_the_basics/chap_3_probl_6.cpp
@@ -7,74 +7,7 @@
 // to digit
 //
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <stdexcept>
-
-bool isNumber(const std::string& s)
-{
-	bool isNumber = true;
-	for (auto&& c : s)
-		isNumber = isNumber && (std::isdigit(c) || c == '.');
-	return isNumber;
-}
-
-std::vector<std::string> splitString(const std::string& input)
-{
-	char delimiter = ' ';
-	if (input.find(delimiter) == std::string::npos)
-		return std::vector<std::string>{ input };
-	std::vector<std::string> result;
-	std::string tmp{};
-	for (auto&& c : input)
-	{
-		if (c != delimiter)
-			tmp += c;
-		else
-		{
-			result.push_back(tmp);
-			tmp = "";
-		}
-	}
-	result.push_back(tmp);
-	return result;
-}
-
-std::string convertNumberToString(const std::string& input,
-	const std::vector<std::string>& spelledDigits)
-{
-	std::string result{};
-	constexpr auto dotPosition = 10;
-	for (auto&& c : input)
-	{
-		if (std::isdigit(c))
-			result += spelledDigits.at(c - '0');
-		else if (c == '.')
-			result += spelledDigits.at(dotPosition);
-		else
-			throw std::invalid_argument(input + " is not a valid number!");
-		result += ' ';
-	}
-	return result;
-}
-
-std::string convertStringToNumbers(const std::string& input,
-	const std::vector<std::string>& spelledDigits)
-{
-	std::string result{};
-	for (auto&& s : splitString(input))
-	{
-		auto it = std::find(spelledDigits.begin(), spelledDigits.end(), s);
-		if (it == spelledDigits.end())
-			throw std::invalid_argument(s + " is not a spelled number!");
-		if (*it == "dot")
-			result += '.';
-		else
-			result += std::to_string(it - spelledDigits.begin());
-	}
-	return result;
-}
+#include "chap_3_probl_6.h"
 
 int main()
 {
---
+++ part_1_the_basics/chap_3_probl_7.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Nikita Elsakov on 07.03.2020.
+// Can perform arithmetic operations on two numbers from 0 to 9
+// Numbers can be spelled-out values. E.g. one two + or 1 2 +
+//
+
+int main()
+{
+
+}
+
