038b26adfc0119557fb7c228f602d4a0a1945d9c
Nikita Elsakov <elsakov@teamidea.ru>
2020-03-22 23:45:41 +0300

    Implement solution for problem 6 of chapter 6.

--- part_1_the_basics/chapter_6/problem_6.cpp
+++ part_1_the_basics/chapter_6/problem_6.cpp
@@ -5,7 +5,7 @@
 /*
 Sentence:
           Noun Verb                                            // e.g., C++ rules
-		  The Noun Verb // e.g. The birds fly
+		  Article Noun Verb // e.g. The birds fly
           Sentence Conjunction Sentence      // e.g., Birds fly but fish swim
 Conjunction:
           "and"
@@ -19,6 +19,8 @@ Verb:
           "rules"
           "fly"
           "swim”
+Article:
+		  "the"
 */
 
 #include <string>
@@ -27,50 +29,79 @@ Verb:
 #include <stdexcept>
 #include <sstream>
 
-bool isValidWord(const std::string& word);
-
 void error(const std::string& msg)
 {
 	throw std::runtime_error(msg);
 }
 
-std::vector<std::string> readSentence()
+std::string readArticle()
 {
-	std::string word{};
-	std::vector<std::string> words{};
-	for (; std::cin >> word;)
+	std::string article{};
+	std::cin >> article;
+	if (article != "the")
 	{
-		if (!isValidWord(word))
-			error(word + " is not valid");
-		words.push_back(word);
-		if (word == ".")
-			break;
+		for (auto it = article.end() - 1; it >= article.begin(); --it)
+			std::cin.putback(*it);
+		article = "";
 	}
-	return words;
+	return article;
 }
 
-bool isValidWord(const std::string& word)
+std::string readNoun()
 {
-	constexpr const char* words[]{
-		"the",
-		"and",
-		"or",
-		"but",
-		"birds",
-		"fish",
-		"C++",
-		"rules",
-		"fly",
-		"swim",
-		"."
-	};
+	auto noun = readArticle();
+	std::stringstream ss{};
+	std::cin >> noun;
+	if (noun == "the")
+		ss << noun << ' ';
+	if (noun == "C++" || noun == "birds" || noun == "fish")
+		ss << noun;
+	else
+		error(noun + " is not a valid noun");
+	return ss.str();
+}
 
-	for (auto&& w : words)
-	{
-		if (w == word)
-			return true;
-	}
-	return false;
+std::string readVerb()
+{
+	const std::vector<std::string> verbs{
+		"swim",
+		"fly",
+		"rules"
+	};
+	std::string verb{};
+	std::cin >> verb;
+	if (std::find(verbs.begin(), verbs.end(), verb) == verbs.end())
+		error(verb + " is not a valid verb");
+	return verb;
+}
+
+std::string readConjunction()
+{
+	const auto conjunctions = {
+		"and",
+		"but",
+		"or"
+	};
+	std::string conjunction{};
+	std::cin >> conjunction;
+	if (conjunction == ".")
+		return conjunction;
+	else if (std::find(conjunctions.begin(), conjunctions.end(), conjunction)
+		== conjunctions.end())
+		error(conjunction + " is not a valid conjunction");
+	return conjunction;
+}
+
+std::string readSentence()
+{
+	std::stringstream ss{};
+	auto noun = readNoun();
+	auto verb = readVerb();
+	auto conjunction = readConjunction();
+	ss << noun << ' ' << verb << ' ' << conjunction;
+	if (conjunction != ".")
+		ss << ' ' << readSentence();
+	return ss.str();
 }
 
 int main()
@@ -78,12 +109,15 @@ int main()
 	std::cout << "Enter a sentence terminated by dot and press enter. "
 			  << "\nThere must be a space character after last word in sentence and dot.\n";
 
-	auto sentence = readSentence();
-	std::stringstream ss{};
-	std::for_each(sentence.begin(), sentence.end(), [&](auto w)
+	try
 	{
-	  ss << w << ' ';
-	});
-	std::cout << ss.str() << std::endl;
+		auto sentence = readSentence();
+		std::cout << "OK" << std::endl;
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return 0;
 }
