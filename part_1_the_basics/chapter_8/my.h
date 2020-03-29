723647436ff1a9d398c3dd919924977510747add
Nikita Elsakov <vocasle@gmail.com>
2020-03-29 14:40:44 +0300

    Fix C26451 warning

---
+++ part_1_the_basics/chapter_8/my.h
@@ -0,0 +1,7 @@
+extern int foo;
+void print_foo();
+void print(int);
+void swap_v(int, int);
+void swap_r(int&, int&);
+// commented out because it won't compile
+//void swap_cr(const int&, const int&);
