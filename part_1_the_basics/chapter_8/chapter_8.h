5c738f154a403b400d1da36f99c9e2bbccf56ed6
Nikita Elsakov <vocasle@gmail.com>
2020-04-01 08:54:33 +0300

    Move ex 11 specific function out of chapter_8.h

--- part_1_the_basics/chapter_8/chapter_8.h
+++ part_1_the_basics/chapter_8/chapter_8.h
@@ -10,83 +10,8 @@ void error(const std::string& msg)
 }
 
 template <typename T>
-void print(const std::string& label, const std::vector<T>& numbers)
+void print(const std::string& label, const std::vector<T>& vec)
 {
-	for (auto&& number : numbers)
-		std::cout << label << ' ' << number << '\n';
-}
-
-struct VectorData
-{
-	double max;
-	double min;
-	double mean;
-	double median;
-};
-
-template<typename T1, typename T2>
-struct Can_copy {
-	static void constraints(T1 a, T2 b) 
-	{ 
-		T2 c = a; 
-		b = a;
-	}
-	Can_copy() 
-	{ 
-		void(*p)(T1, T2) = constraints; 
-	}
-};
-
-template <typename T>
-VectorData get_vector_data(const std::vector<T>& numbers)
-{
-	if (numbers.size() == 0)
-		error("vector is emtpy");
-	// consult http://www.stroustrup.com/bs_faq2.html#constraints
-	Can_copy<T, double>(); // accept only numeric types
-	auto it = numbers.begin();
-	auto vd = VectorData();
-	vd.max = *it;
-	vd.min = *it;
-	double sum = *it;
-
-	if (numbers.size() == 1)
-	{
-		vd.min = vd.median = vd.mean = vd.max;
-		return vd;
-	}
-
-	for (it = it + 1; it != numbers.end(); ++it)
-	{
-		if (vd.max < *it)
-			vd.max = *it;
-		if (vd.min > * it)
-			vd.min = *it;
-		sum += *it;
-	}
-
-	std::vector<T> copy_of_v(numbers);
-	std::sort(copy_of_v.begin(), copy_of_v.end());
-
-	vd.mean = sum / numbers.size();
-	size_t pos = numbers.size() / 2;
-	if (numbers.size() % 2 == 0)
-	{
-		double upper = numbers.at(pos);
-		double lower = numbers.at(pos - 1);
-		vd.median = (upper + lower) / 2.0;
-	}
-	else
-		vd.median = numbers.at(pos);
-
-	return vd;
-}
-
-void print_vector_data(const VectorData& vd)
-{
-	std::cout << "Max element: " << vd.max
-		<< "\nMin element: " << vd.min
-		<< "\nMedian: " << vd.median
-		<< "\nMean: " << vd.mean
-		<< '\n';
+	for (auto&& el : vec)
+		std::cout << label << ' ' << el << '\n';
 }
--- part_1_the_basics/chapter_8/problem_11.cpp
+++ part_1_the_basics/chapter_8/problem_11.cpp
@@ -8,6 +8,101 @@
 
 #include "chapter_8.h"
 
+struct VectorData
+{
+	double max;
+	double min;
+	double mean;
+	double median;
+};
+
+template<typename T1, typename T2>
+struct Can_copy {
+	static void constraints(T1 a, T2 b)
+	{
+		T2 c = a;
+		b = a;
+	}
+	Can_copy()
+	{
+		void(*p)(T1, T2) = constraints;
+	}
+};
+template <typename Number>
+VectorData find_max_min_mean(const std::vector<Number>& numbers)
+{
+	VectorData vd;
+
+	auto it = numbers.begin();
+	vd.max = *it;
+	vd.min = *it;
+	double sum = *it;
+
+	if (numbers.size() == 1)
+	{
+		vd.min = vd.mean = vd.max;
+		return vd;
+	}
+
+	for (it = it + 1; it != numbers.end(); ++it)
+	{
+		if (vd.max < *it)
+			vd.max = *it;
+		if (vd.min > * it)
+			vd.min = *it;
+		sum += *it;
+	}
+
+	vd.mean = sum / numbers.size();
+	return vd;
+}
+
+template <typename Number> 
+void find_median(const std::vector<Number>& numbers,
+	VectorData& vd)
+{
+	if (numbers.size() == 1)
+	{
+		vd.median = numbers.at(0);
+		return;
+	}
+
+	std::vector<Number> numbers_copy(numbers);
+	std::sort(numbers_copy.begin(), numbers_copy.end());
+
+	size_t pos = numbers.size() / 2;
+	vd.median = numbers.at(pos);
+
+	if (numbers.size() % 2 == 0)
+	{
+		double upper = numbers.at(pos);
+		double lower = numbers.at(pos - 1);
+		vd.median = (upper + lower) / 2.0;
+	}
+}
+
+
+template <typename Number>
+VectorData get_vector_data(const std::vector<Number>& numbers)
+{
+	if (numbers.size() == 0)
+		error("vector is emtpy");
+	// consult http://www.stroustrup.com/bs_faq2.html#constraints regarding Can_copy
+	Can_copy<Number, double>(); // accept only numeric types
+	auto vd = find_max_min_mean(numbers);
+	find_median(numbers, vd);
+	return vd;
+}
+
+void print_vector_data(const VectorData& vd)
+{
+	std::cout << "Max element: " << vd.max
+		<< "\nMin element: " << vd.min
+		<< "\nMedian: " << vd.median
+		<< "\nMean: " << vd.mean
+		<< '\n';
+}
+
 int main()
 {
 	try
