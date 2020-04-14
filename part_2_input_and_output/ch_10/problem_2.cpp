cd12846d58d27b12b2aad5be7ade9dc367958c6b
Nikita Elsakov <vocasle@gmail.com>
2020-04-14 23:01:37 +0300

    Ch10,Ex4: Update problem_2.cpp, problem_3.cpp to solve ex4 of ch10

--- part_2_input_and_output/ch_10/problem_2.cpp
+++ part_2_input_and_output/ch_10/problem_2.cpp
@@ -13,18 +13,24 @@ double get_random_temp(double min, double max)
 	return urd(re);
 }
 
-uint32_t get_random_hour()
+int32_t get_random_int(int32_t from, int32_t to)
 {
 	std::random_device rd;
-	std::uniform_int_distribution<uint32_t> uid(0, 23);
+	std::uniform_int_distribution<uint32_t> uid(from, to);
 	std::default_random_engine re{ rd() };
 	return uid(re);
+}
+
+uint32_t get_random_hour()
+{
+	return get_random_int(0, 23);
 }
 
 struct Reading
 {
 	uint32_t hour;
 	double temp;
+	char temp_unit;
 };
 
 Reading generate_reading(double temp_min = -60, double temp_max = 50)
@@ -32,6 +38,11 @@ Reading generate_reading(double temp_min = -60, double temp_max = 50)
 	Reading r{};
 	r.hour = get_random_hour();
 	r.temp = get_random_temp(temp_min, temp_max);
+	r.temp_unit = get_random_int(0, 1) == 0 ? 'c' : 'f';
+	if (r.temp_unit == 'f')
+	{
+		r.temp = r.temp * (9.0 / 5) + 32;
+	}
 	return r;
 }
 
@@ -55,7 +66,7 @@ void write_readings_to_file(const std::string& file_name,
 	}
 	for (auto& reading : readings)
 	{
-		ofs << reading.hour << ' ' << reading.temp << '\n';
+		ofs << reading.hour << ' ' << reading.temp << reading.temp_unit << '\n';
 	}
 }
 
--- part_2_input_and_output/ch_10/problem_3.cpp
+++ part_2_input_and_output/ch_10/problem_3.cpp
@@ -9,6 +9,7 @@ struct Reading
 {
 	uint32_t hour;
 	double temp;
+	char temp_unit;
 };
 
 bool operator==(const Reading& lhs, const Reading& rhs)
@@ -35,18 +36,38 @@ std::istream& operator>>(std::istream& is, Reading& r)
 {
 	uint32_t hour{ 0 };
 	double temp{ 0 };
-	is >> hour >> temp;
+	char temp_unit;
+	is >> hour >> temp >> temp_unit;
 	if (!is)
 	{
 		return is;
 	}
+	if (!(temp_unit == 'f' || temp_unit == 'c'))
+	{
+		throw std::runtime_error(std::string(1, temp_unit) 
+			+ " is not a valid temperature unit.");
+	}
 	r.hour = hour;
 	r.temp = temp;
+	r.temp_unit = temp_unit;
 	return is;
+}
+
+double celc_to_fahr(double c)
+{
+	return c * (9 / 5.0) + 32;
+}
+
+double fahr_to_celc(double f)
+{
+	return (f - 32) / (9 / 5.0);
 }
 
 std::vector<Reading> get_readings_from_file(const std::string& file_name)
 {
+	static const auto invalid_temp{ UINT32_MAX };
+	static const auto invalid_hour{ 25 };
+	static const auto invalid_temp_unit{ 'k' };
 	std::ifstream ifs{ file_name };
 	if (!ifs)
 	{
@@ -56,9 +77,14 @@ std::vector<Reading> get_readings_from_file(const std::string& file_name)
 	Reading r;
 	while (ifs >> r && !ifs.eof())
 	{
+		if (r.temp_unit == 'f')
+		{
+			r.temp = fahr_to_celc(r.temp);
+		}
 		readings.push_back(r);
-		r.temp = 0;
-		r.hour = 0;
+		r.temp = invalid_temp;
+		r.hour = invalid_hour;
+		r.temp_unit = invalid_temp_unit;
 	}
 	return readings;
 }
