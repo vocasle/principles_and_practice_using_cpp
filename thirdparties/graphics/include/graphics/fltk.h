5760d14334e57d346646b2e9c9e03d8f1c7aed42
Nikita Elsakov <vocasle@gmail.com>
2020-05-05 11:52:57 +0300

    Add graphics support lib

---
+++ thirdparties/graphics/include/graphics/fltk.h
@@ -0,0 +1,17 @@
+#ifndef FLTK_GUARD
+#define FLTK_GUARD 1
+
+#include <FL/Fl.h>
+#include <FL/Fl_Window.h>
+#include <FL/Fl_Button.h>
+#include <FL/Fl_Input.h>
+#include <FL/Fl_Output.h>
+#include <cstdlib>       // for exit(0)
+#include <FL/fl_draw.h>
+#include <FL/Enumerations.h>
+
+#include <Fl/Fl_JPEG_Image.h>
+#include <Fl/Fl_GIF_Image.h>
+
+#endif
+
